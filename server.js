46e515ed06fe5d89d8e69d68244b1bd838a34dc3
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-10 01:21:01 -0700

    connect mongoose

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(require('./routes'))
 
+mongoose.connect(process.env.MONGO_URI) ||
+  'mongodb://localhost/social-media-api',
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true)
 
-mongoose.connect(process.env.MONGO_URI) || 'mongodb://localhost/'
-
-
+app.listen(PORT, () => console.log(`connected to ${PORT}`))
