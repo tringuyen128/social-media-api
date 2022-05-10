d2e271d7cd951a4759741fd88aaa98d4352db20c
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-10 14:03:35 -0700

    complete

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -6,6 +10,7 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(require('./routes'))
 
 mongoose.connect(process.env.MONGO_URI) ||
   'mongodb://localhost/social-media-api',
