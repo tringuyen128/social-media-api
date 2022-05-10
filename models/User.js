cb76b0c86b092b6a5b94e1b8ecf3cc92623eb78e
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-05-10 01:05:20 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose')
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+)
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
